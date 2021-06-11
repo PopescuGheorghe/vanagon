694c5cbb76c6b3eac425243a9e1dc955f343886c
gimmy <gheorghe.popescu@puppet.com>
2021-06-11 12:35:59 +0300

    (PA-3755) use rsync3 on mac OS >= 11
    mac OS >= 11 comes with rsync 3 while older versions
    have rsyn 2.6.9 installed. To ensure that the signature
    of the files is maintained, we need to preserve
    extended attributes. On rsync 2 we need to use the
    `--extended-attributes` flag while on rsync 3 we
    need to use `--xattrs` and specify to the remote host
    to use rsync 3 using `--rsync-path`

--- lib/vanagon/utilities/extra_files_signer.rb
+++ lib/vanagon/utilities/extra_files_signer.rb
@@ -19,11 +19,12 @@ class Vanagon
             remote_host = "#{project.signing_username}@#{project.signing_hostname}"
             remote_destination_path = "#{remote_host}:#{tempdir}"
             remote_file_location = "#{remote_host}:#{file_location}"
+            ex_flags = extra_flags(project.platform)
 
             commands += [
-              "rsync -e '#{Vanagon::Utilities.ssh_command}' --verbose --recursive --hard-links --links  --no-perms --no-owner --no-group #{local_source_path} #{remote_destination_path}",
-              "#{Vanagon::Utilities.ssh_command} #{remote_host} #{project.signing_command} #{file_location}",
-              "rsync -e '#{Vanagon::Utilities.ssh_command}' --verbose --recursive --hard-links --links  --no-perms --no-owner --no-group #{remote_file_location} #{local_source_path}"
+              "rsync -e '#{Vanagon::Utilities.ssh_command}' --verbose --recursive --hard-links --links  --no-perms --no-owner --no-group #{ex_flags} #{local_source_path} #{remote_destination_path}",
+              "#{Vanagon::Utilities.ssh_command} #{remote_host} '#{project.signing_command} #{file_location}'",
+              "rsync -e '#{Vanagon::Utilities.ssh_command}' --verbose --recursive --hard-links --links  --no-perms --no-owner --no-group #{ex_flags} #{remote_file_location} #{local_source_path}"
             ]
           end
 
@@ -32,6 +33,18 @@ class Vanagon
           require 'vanagon/logger'
           VanagonLogger.error "Unable to connect to #{project.signing_username}@#{project.signing_hostname}, skipping signing extra files: #{project.extra_files_to_sign.join(',')}"
           []
+        end
+
+        private
+
+        def extra_flags(platform)
+          return '' unless platform.is_macos?
+
+          if platform.os_version =~ /^10\./
+            '--extended-attributes'
+          else
+            '--rsync-path=/usr/local/bin/rsync3 --xattrs'
+          end
         end
       end
     end
